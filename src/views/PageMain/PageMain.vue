f960b65ae72facd824a1c0937bcc87c75b408105
Andrey Gaisinskii <andygaisinskii@gmail.com>
2019-09-02 22:48:55 +0300

    all preps are done

---
+++ src/views/PageMain/PageMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page_main">
+    <h1 class="page__heading">
+      main page is here
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.page_main {
+  .page {
+    &__heading {
+      font-size: 22px;
+      color: $testvar;
+    }
+  }
+}
+</style>
